f133e2839d8951f9d2a8412276afd229bc9f19e6
adi bangkit <adibangkai@gmail.com>
2024-01-22 16:48:04 +0700

    sidebar menu setting

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log("connected");
+var coll = document.getElementsByClassName("side-title");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var sideNav = this.nextElementSibling;
+    sideNav.classList.toggle("hidden");
+  });
+}
